aaffe986a7ea21597b27f1e3d90d209ff61afe5c
Lloyd Deng <lllloyd@uw1-320-00.uwb.edu>
2019-10-22 18:01:15 -0700

    v0.2a: Complete server-client infrastructure

--- Battleship/Client.cpp
+++ Battleship/Client.cpp
@@ -1,25 +1,149 @@
+#include <sys/types.h>  
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h>  
+#include <netdb.h>      
+#include <unistd.h>     
+#include <strings.h>    
+#include <netinet/tcp.h>
+#include <sys/uio.h>    
+#define YOUR_ID 6932    
+#define SERVER_NAME "localhost"
+#define REPETITION 20000
+#define NBUFS 3
+#define BUFSIZE 500
+#define TYPE 3
+
+#include <string>
 #include <iostream>
+#include <sys/time.h>
 
-using namespace std;
+bool bad_server_port(int server_port)
+{
+	if (server_port < 1024 || server_port > 9999)
+	{
+		std::cout << "valid server_port numbers are between 1024 and 9999" << std::endl;
+		return true;
+	}
+	else return false;
+}
 
-int main( ) {
-  
-  
-  cout << "enter server_port: ";
-  cin >> server_port;
-  cout << "enter server_name: ";
-  cin >> server_name;
-  
-  struct hostent* host = gethostbyname( server_name );
+bool bad_server_name(const char* server_name)
+{
+	return false;
+}
 
-  int port = YOUR_ID;  // the last 4 digits of your student id
-  sockaddr_in sendSockAddr;
-  bzero( (char*)&sendSockAddr, sizeof( sendSockAddr ) );
-  sendSockAddr.sin_family      = AF_INET; // Address Family Internet
-  sendSockAddr.sin_addr.s_addr =
-  inet_addr( inet_ntoa( *(struct in_addr*)*host->h_addr_list ) );
-  sendSockAddr.sin_port        = htons( server_port );
-  
-  int clientSd = socket( AF_INET, SOCK_STREAM, 0 );
-  connect( clientSd, ( sockaddr* )&sendSockAddr, sizeof( sendSockAddr ) );
+bool bad_repetition(int repetition)
+{
+	if (repetition < 0)
+	{
+		std::cout << "valid repetition numbers are greater than 0" << std::endl;
+		return true;
+	}
+	else return false;
+}
+
+bool bad_nbufs(int nbufs)
+{
+	if (nbufs < 0)
+	{
+		std::cout << "valid nbufs numbers are greater than 0" << std::endl;
+		return true;
+	}
+	else return false;
+}
+
+bool bad_bufsize(int bufsize)
+{
+	if (bufsize < 0)
+	{
+		std::cout << "valid bufsize numbers are greater than 0" << std::endl;
+		return true;
+	}
+	else return false;
+}
+
+bool bad_nbufs_bufsize(int nbufs, int bufsize)
+{
+	if (nbufs * bufsize != 1500)
+	{
+		std::cout << "valid nbufs * bufsize == 1500" << std::endl;
+		return true;
+	}
+	else return false;
+}
+
+bool bad_type(int type)
+{
+	if (type < 1 || type > 4)
+	{
+		std::cout << "valid type numbers are between 1 and 3" << std::endl;
+		return true;
+	}
+	return false;
+}
+
+int main(int argc, char const *argv[]) 
+{
+	int server_port = YOUR_ID;
+	if (argc > 1) server_port = atoi(argv[1]);
+	const char* server_name = SERVER_NAME;
+	if (argc > 2) server_name = argv[2];
+	int repetition = REPETITION;
+	if (argc > 3) repetition = atoi(argv[3]);
+	int nbufs = NBUFS;
+	if (argc > 4) nbufs = atoi(argv[4]);
+	int bufsize = BUFSIZE;
+	if (argc > 5) bufsize = atoi(argv[5]);
+	bad_nbufs_bufsize(nbufs, bufsize);
+	int type = TYPE;
+	if (argc > 6) type = atoi(argv[6]);
+	
+	if (argc > 7 || bad_server_port(server_port) || bad_server_name(server_name) || bad_repetition(repetition) || bad_nbufs(nbufs) || bad_bufsize(bufsize) || bad_type(type))
+	{
+		std::cout << "Usage: ./client.out [server_port] [server_name] [repetition] [nbufs] [bufsize] [type]" << std::endl;
+	}
+	char databuf[nbufs][bufsize];
+	std::string alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+	for (int i = 0; i < nbufs; i++) {
+		for (int j = 0; j < bufsize; j++) {
+			databuf[i][j] = alphabet[j % alphabet.size()];
+		}
+	}
+	struct hostent* host = gethostbyname( server_name );
+    sockaddr_in sendSockAddr;
+    bzero( (char*)&sendSockAddr, sizeof( sendSockAddr ) );
+    sendSockAddr.sin_family      = AF_INET;
+    sendSockAddr.sin_addr.s_addr = inet_addr( inet_ntoa( *(struct in_addr*)*host->h_addr_list ) );
+    sendSockAddr.sin_port        = htons( server_port );
+	int clientSd = socket( AF_INET, SOCK_STREAM, 0 );
+	connect( clientSd, ( sockaddr* )&sendSockAddr, sizeof( sendSockAddr ) );
+	struct timeval start, lap, stop;
+	gettimeofday(&start, NULL);
+	for (int i = 0; i < repetition; i++)
+	{
+		if (type == 1)
+		{
+			for ( int j = 0; j < nbufs; j++ )			
+			write( clientSd, databuf[j], bufsize );
+		}
+		else if (type == 2)
+		{
+			struct iovec vector[nbufs];
+			for ( int j = 0; j < nbufs; j++ ) {
+			vector[j].iov_base = databuf[j];
+			vector[j].iov_len = bufsize;
+			}
+			writev( clientSd, vector, nbufs );
+		}
+		else if (type == 3)
+		{
+			write( clientSd, databuf, nbufs * bufsize );
+		}
+	}
+	gettimeofday(&lap, NULL);
+	int count;
+	read( clientSd , &count , sizeof(count) ); 
+	gettimeofday(&stop, NULL);
+	close(clientSd);
 }
