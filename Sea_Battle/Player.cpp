61aca36bab0f967d2301cbb216ac51018d54ad89
Lloyd Deng <lllloyd@uw1-320-00.uwb.edu>
2019-12-06 03:42:39 -0800

    v2.0b Bugfix for immediate crash depending on time of day

--- Sea_Battle/Player.cpp
+++ Sea_Battle/Player.cpp
@@ -2,6 +2,7 @@
 CSS 432 AU19
 Sea Battle - Intro to Network Programming
 Team Cookies
+
 	Build: g++ -o player.out Player.cpp -pthread
 	Usage: ./player.out [iport] [uport] [uaddress]
     Example: ./player.out 6932 2396 localhost
@@ -26,6 +27,11 @@ Team Cookies
 #define DEFAULT_NAME "localhost"
 #define DEFAULT_PORT 6932
 
+int iPort;
+std::string iAddress;
+int uPort;
+std::string uAddress;
+
 // Setup: Connects one peer to another. Each peer has both IP and port of both peers.
 
 // Turn: Each peer will listen for requests and user input, whichever comes first.
@@ -36,6 +42,16 @@ Team Cookies
 // cout << "Waiting for the enemy battleship." << endl;
 // When a request comes in (eg. a1), the sender will mark its board and send a response (eg. Hit!).
 // Once it recognizes that a user input and request has been completed, it will print both boards.
+
+int Player::getIPort( ) { return iPort; }
+std::string Player::getIAddress( ) { return iAddress; }
+int Player::getUPort( ) { return uPort; }
+std::string Player::getUAddress( ) { return uAddress; }
+
+void Player::setIPort( int i ) { iPort = i; }
+void Player::setIAddress( std::string i ) { iAddress = i; }
+void Player::setUPort( int u ) { uPort = u; }
+void Player::setUAddress( std::string u ) { uAddress = u; }
 
 void Player::playerConnect( int clientSd, sockaddr_in sendSockAddr ) {
 	connect( clientSd, ( sockaddr* )&sendSockAddr, sizeof( sendSockAddr ) );
@@ -65,15 +81,26 @@ void Player::playerWrite( int clientSd, std::string requestString ) {
 	write(clientSd, request, sizeof(request)); //single write
 }
 
-int Player::main( int argc, char *argv[] ) {
+int main( int argc, char *argv[] ) {
+    Player::setIPort( 0 );
+    Player::setUPort ( -1 );
+    Player::setIAddress("localhost");
+    Player::setUAddress("localhost");
+
+	if (argc == 4){
+		Player::setIPort(std::stoi(argv[1]));
+		Player::setUPort(std::stoi(argv[2]));
+		Player::setUAddress(argv[3]);
+	}
+
+
     ///0: Set up the remote connection
 
-    int iport = std::stoi(argv[1]);
-    int uport = std::stoi(argv[2]);
-    std::string uaddress = argv[3];
+
     
     /** //TODO: Proper argument handling
 	std::string server_name;
+
     if (argc == 1) {
         cerr << "TODO: Launch Server using default ip and port";
     }
@@ -88,13 +115,13 @@ int Player::main( int argc, char *argv[] ) {
     }**/
 
     std::cout << "\nPreparing local resources..." << std::flush; // cout must flush for futures compatibility
-    struct hostent* host = gethostbyname( uaddress.c_str() );
+    struct hostent* host = gethostbyname( Player::getUAddress().c_str() );
     sockaddr_in sendSockAddr;
     bzero( (char*)&sendSockAddr, sizeof( sendSockAddr ) );
     sendSockAddr.sin_family      = AF_INET;
     sendSockAddr.sin_addr.s_addr =
       inet_addr( inet_ntoa( *(struct in_addr*)*host->h_addr_list ) );
-    sendSockAddr.sin_port        = htons( uport );
+    sendSockAddr.sin_port        = htons( Player::getUPort() );
 	int clientSd = Etc::errChk(socket( AF_INET, SOCK_STREAM, 0 ), "Error: Opening stream socket");
     //	Etc::errChk(connect( clientSd, ( sockaddr* )&sendSockAddr, sizeof( sendSockAddr ) ), "Error: Socket invalid");
 
@@ -102,7 +129,7 @@ int Player::main( int argc, char *argv[] ) {
     bzero( (char*)&acceptSockAddr, sizeof( acceptSockAddr ) );
     acceptSockAddr.sin_family      = AF_INET;
     acceptSockAddr.sin_addr.s_addr = htonl( INADDR_ANY );
-    acceptSockAddr.sin_port        = htons( iport );
+    acceptSockAddr.sin_port        = htons( Player::getIPort() );
     int serverSd = Etc::errChk(socket( AF_INET, SOCK_STREAM, 0 ), "Error: Stream-oriented socket failed to open.");
     const int on = 1;
     Etc::errChk(setsockopt( serverSd, SOL_SOCKET, SO_REUSEADDR, (char *)&on, 
@@ -110,9 +137,8 @@ int Player::main( int argc, char *argv[] ) {
     Etc::errChk(bind( serverSd, ( sockaddr* )&acceptSockAddr, sizeof( acceptSockAddr ) ), "Error: Socket failed to bind.");
     Etc::errChk(listen( serverSd, 5 ), "Error: Operating system failed to listen.");
 	sockaddr_in newSockAddr;
-    int newSd; // = Etc::errChk(accept( serverSd, ( sockaddr *)&newSockAddr, &newSockAddrSize ), "Error: Socket failed to accept.");
+    int newSd = -1; // = Etc::errChk(accept( serverSd, ( sockaddr *)&newSockAddr, &newSockAddrSize ), "Error: Socket failed to accept.");
     std::cout << "\nLocal resources prepared!" << std::flush;
-
 	std::this_thread::sleep_for(std::chrono::seconds(1));
 
     std::cout << "\nEstablishing remote connection..." << std::flush; // cout does not work with futures until output sequence is flushed
@@ -120,11 +146,11 @@ int Player::main( int argc, char *argv[] ) {
     https://gist.github.com/vmrob/ff20420a20c59b5a98a1 */
     std::future<void> playerConnect = std::async(std::launch::async, Player::playerConnect, clientSd, sendSockAddr); // TODO: Redundant initialization,
     std::future<int> playerAccept = std::async(std::launch::async, Player::playerAccept, serverSd, newSockAddr); // since this is repeated in the loop?
-    
-    while (newSd == 0) {
+
+    while (newSd == -1) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
         std::cout << "." << std::flush;
-        
+		
         if (playerConnect.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
             playerConnect = std::async(std::launch::async, Player::playerConnect, clientSd, sendSockAddr);
         }
@@ -134,12 +160,11 @@ int Player::main( int argc, char *argv[] ) {
             playerAccept = std::async(std::launch::async, Player::playerAccept, serverSd, newSockAddr);
         }
     }
-
     std::cout << "\nRemote connection established!" << std::endl;
 	std::this_thread::sleep_for(std::chrono::seconds(1));
-	std::cout << "iport: " << iport << std::endl;
-	std::cout << "uaddress: " << uaddress << " uport: " << uport << std::endl;
-    std::cout << "clientSd: " << clientSd << "newSd: " << newSd << std::endl;
+	std::cout << "iport: " << Player::getIPort() << " iaddress: " << Player::getIAddress() << std::endl;
+	std::cout << "uport: " << Player::getUPort() << " uaddress: " << Player::getUAddress() << std::endl;
+    std::cout << "clientSd: " << clientSd << " serverSd: " << serverSd << " newSd: " << newSd << std::endl;
 	std::cout << std::endl << std::endl << std::endl << std::endl << std::endl << std::endl << std::endl << std::endl << std::endl << std::endl;
 	std::cout << "Welcome to Sea Battle, a game by Team Cookies\n" << std::endl;
 
@@ -167,14 +192,13 @@ int Player::main( int argc, char *argv[] ) {
         std::future<std::string> playerRead = std::async(std::launch::async, Player::playerRead, newSd); // when updated in the loop?
         std::future<void> playerWrite = std::async(std::launch::async, Player::playerWrite, clientSd, mapTargetCoords);
 
-        while (mapTargetCoords.empty() || fieldTargetCoords.empty()) {
+        while (mapTargetCoords.empty() || fieldTargetCoords.empty()) { /// Until both have been set
             //if (!mapTargetCoords.empty()) { std::cout << " Waiting for opponent to make a move. " << std::flush; } //TODO: Fancify this ui
             //else if (!fieldTargetCoords.empty()) { std::cout << "Opponent is waiting for you to make a move!" << std::flush; }
             //else { std::cout << "Debugging"; }
             std::this_thread::sleep_for(std::chrono::seconds(1)); // One move allowed per second
-            //std::cout << "." << std::flush;
 
-            if (playerCin.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
+            if (playerCin.wait_for(std::chrono::seconds(0)) == std::future_status::ready) { /// Set mapTargetcoords from Player
                 mapTargetCoords = playerCin.get();
                 playerCin = std::async(std::launch::async, Player::playerCin);
 
@@ -182,12 +206,11 @@ int Player::main( int argc, char *argv[] ) {
 	            //write(clientSd, mapTargetCoordsPointer, sizeof(mapTargetCoordsPointer)); //single write
                 playerWrite = std::async(std::launch::async, Player::playerWrite, clientSd, mapTargetCoords); // Is using future on this line necessary if not blocking program
             }
-            if (playerRead.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
+            if (playerRead.wait_for(std::chrono::seconds(0)) == std::future_status::ready) { /// Set fieldTargetCoords from port
                 fieldTargetCoords = playerRead.get();
                 playerRead = std::async(std::launch::async, Player::playerRead, newSd);
             }
         }
-
         std::string fieldTargetStatus = field.attackBoard(field.requestTranslator(fieldTargetCoords[0]), field.requestTranslator(fieldTargetCoords[1])); // Assumes coords are at pos 0 and 1
         std::string mapTargetStatus;
         std::future<void> clientWrite = std::async(std::launch::async, Player::playerWrite, clientSd, fieldTargetStatus); // TODO: Redundant initialization,
@@ -196,7 +219,6 @@ int Player::main( int argc, char *argv[] ) {
         while (mapTargetStatus.empty()) {
             //Alternatively to threading, we could implement style of Aloha which waits for a random amount of time to send, and listen for the rest of the time.
             std::this_thread::sleep_for(std::chrono::seconds(1)); // One move allowed per second
-            //std::cout << "." << std::flush;
             //std::cout << "send: " << send << "recieve: " << recieve << std::endl;
 
             if (clientWrite.wait_for(std::chrono::seconds(0)) == std::future_status::ready) { // TODO: is this necessary, since write does not block the program?
@@ -222,5 +244,4 @@ int Player::main( int argc, char *argv[] ) {
     }
 // TODO: win conditions
     std::cerr << "Until updated, this program should never reach here.";
-    //TODO: Socket termination
 }
